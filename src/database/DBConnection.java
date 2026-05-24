ba6ad9cdee5fac0271dd2493e14e76e7882302b6
lawmiee2101 <redbelocura58@gmail.com>
2026-05-25 01:26:36 +0800

    Design for table, Arrangment of data

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/student_record_db";
-    private static final String USER = "INPUT_YOUR_USERNAME_HERE";
-    private static final String PASSWORD = "INPUT_YOUR_PASSWORD_HERE";
+    private static final String USER = "admin";
+    private static final String PASSWORD = "admin123";
 
     public static Connection getConnection() {
         try {
